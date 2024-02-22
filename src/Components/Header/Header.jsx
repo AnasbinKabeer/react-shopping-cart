b675a82f380838c3f42043025e959ad9c73d1f1c
AnasbinKabeer <hafizapk007@gmail.com>
2024-02-22 10:01:37 +0530

    First Commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header_container'>
+      <h1>Furistic.in</h1>
+      <h2>Admin</h2>
+    </div>
+  )
+}
+
+export default Header
