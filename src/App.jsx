3ea5bbc01cfc6e04a9f65e8592b51d1473c37991
AnasbinKabeer <hafizapk007@gmail.com>
2024-02-27 11:24:04 +0530

    Checkout

--- src/App.jsx
+++ src/App.jsx
@@ -9,110 +9,113 @@ import Products from "./Components/Products/Products";
 import Filter from './Components/Filter/Filter';
 import Cartdiv from './Components/Cart/Cartdiv';
 
- class App extends React.Component {
-constructor(){
-  super();
-  this.state={
-    products: data.products,
-    size:"",
-    sort:"",
-    cartItems:[]
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+      cartItems: JSON.parse(localStorage.getItem('cartItems')) ? JSON.parse(localStorage.getItem('cartItems')) : []
 
-  }
-}
-
-removeFromCart=(product)=>{
-  const cartItems = this.state.cartItems.slice();
-  this.setState({
-    cartItems: cartItems.filter((x) => x._id !== product._id)
-  }
-  
-  )
-    
- 
-}
-
-addToCart = (product) => {
-  const cartItems = this.state.cartItems.slice();
-  let alreadyInCart = false;
-
-  cartItems.forEach((item) => 
-  {
-    if (item._id === product._id) {
-      alreadyInCart = true;
-      item.count++;
-  
     }
   }
-  );
 
-  if (!alreadyInCart) {
-    cartItems.push({ ...product, count: 1 });
+  createOrder = (order) => {
+    alert("need to save order for" + order.name)
+
   }
 
-  this.setState({
-    cartItems,
-  });
-
-};
-
-
-
-sortProducts=(e)=>{
-  const sort = e.target.value
-  this.setState((state)=>({
-    sort: sort,
-products: this.state.products.slice().sort((a,b)=>(
-  sort === "lowest" ? ((a.price > b.price) ? 1:-1):
-  sort === "higest" ? ((a.price > b.price) ? 1:-1):
- ((a._id > b._id) ? 1:-1)
-))
-  }))
-  console.log(e.target.value)
-
-}
-filterProducts=(e)=>{
-
-  if(e.target.value === ""){
-
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
     this.setState({
-      products: data.products,
-      size: e.target.value
+      cartItems: cartItems.filter((x) => x._id !== product._id)
     })
-  }else{
-
-    this.setState({
-      size: e.target.value,
-      products: data.products.filter(
-        (value) => value.availableSizes.indexOf(e.target.value) >= 0),
-    })
+    localStorage.setItem("cartItems",
+      JSON.stringify(cartItems.filter((x) => x._id !== product._id))
+    )
   }
-}
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        alreadyInCart = true;
+        item.count++;
+
+      }
+    }
+    );
+
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+
+    this.setState({ cartItems });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+
+  };
+
+
+
+
+  sortProducts = (e) => {
+    const sort = e.target.value
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products.slice().sort((a, b) => (
+        sort === "lowest" ? ((a.price > b.price) ? 1 : -1) :
+          sort === "higest" ? ((a.price > b.price) ? 1 : -1) :
+            ((a._id > b._id) ? 1 : -1)
+      ))
+    }))
+    console.log(e.target.value)
+
+  }
+  filterProducts = (e) => {
+
+    if (e.target.value === "") {
+
+      this.setState({
+        products: data.products,
+        size: e.target.value
+      })
+    } else {
+
+      this.setState({
+        size: e.target.value,
+        products: data.products.filter(
+          (value) => value.availableSizes.indexOf(e.target.value) >= 0),
+      })
+    }
+  }
   render() {
     return (
       <div>
-        <Header/>
-     
+        <Header />
+
 
 
         <div className="content">
           <div className="main">
-          <Filter count={this.state.products.length}
-          size={this.state.size}
-          sort={this.state.sort}
-          filterProducts={this.filterProducts}
-          sortProducts={this.sortProducts}/>
-         
-            <Products products={this.state.products} 
-             addToCart={this.addToCart}/>
+            <Filter count={this.state.products.length}
+              size={this.state.size}
+              sort={this.state.sort}
+              filterProducts={this.filterProducts}
+              sortProducts={this.sortProducts} />
+
+            <Products products={this.state.products}
+              addToCart={this.addToCart} />
           </div>
-       
-          
+
+
           <div className="sidebar">
-            <Cartdiv cartItems = {this.state.cartItems}
-            removeFromCart ={this.removeFromCart}/>
+            <Cartdiv createOrder={this.createOrder} cartItems={this.state.cartItems}
+              removeFromCart={this.removeFromCart} />
           </div>
-         
+
         </div>
 
 
--- src/Components/Cart/Cartdiv.jsx
+++ src/Components/Cart/Cartdiv.jsx
@@ -3,7 +3,31 @@ import React, { Component } from 'react'
 import './Cart.css'
 
 export default class Cartdiv extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "",
+      email:"",
+      address: "",
+      showCheckout:false};
+  }
  
+  handleInput =(e)=> {
+    this.setState({ [e.target.name]: e.target.value})
+  }
+
+
+createOrder=(e)=>{
+  e.preventDefault()
+  const order={
+    name: this.state.name,
+    email: this.state.email,
+    address: this.state.address,
+    cartItems: this.props.cartItems
+  }
+  this.props.createOrder(order)
+}
+
   render() {
     const {cartItems} = this.props;
     return (
@@ -47,9 +71,34 @@ export default class Cartdiv extends Component {
           <div>Total: {" "}₹{cartItems.reduce((a, c) => a + (c.price * c.count), 0)}</div>
 
           </div>
-          <button className='button primary'>Proceed</button>
+          <button onClick={()=>{this.setState({showCheckout:true})}} className='button primary'>Proceed</button>
         </div>
-        }
+       
+       }
+       {this.state.showCheckout && (
+        
+        
+        <div className=''>
+        <form action="" onSubmit={this.createOrder}>
+          <ul className='form-Container'>
+            <li>
+            
+              <label  htmlFor="email" >Email</label>
+              <input type="email" name='email'  required onChange={this.handleInput}/>
+            
+              <label  htmlFor="name" >Name</label>
+              <input type="text" name='name'  required onChange={this.handleInput}/>
+
+              <label  htmlFor="Adrdess" >Address</label>
+              <input type="text" name='Address'  required onChange={this.handleInput}/>
+            </li>
+            <li>
+              <button type='sumit' onClick={this.props.createOrder} className='button primary'>Checkout</button>
+            </li>
+          </ul>
+        </form>
+        </div>
+       )}
        </div>
        </div>
 
